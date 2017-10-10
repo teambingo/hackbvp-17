93b3cfc49eea13c93028f642b2ac50c1e126217c
aniket965 <aniket965.as@gmail.com>
2017-10-11 01:03:57 +0530

    Completed UI webserver ui 👊🕶 👊🕶

--- webuiserver/src/App.js
+++ webuiserver/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import {
   Segment,
   Grid,
+  Card,
+  Image,
+  Button,
   Statistic
 } from 'semantic-ui-react'
 class App extends Component {
@@ -9,31 +12,88 @@ class App extends Component {
     return (
       <div className="App">
         <Grid container fluid>
-          <Grid.Row/>
+          <Grid.Row />
           <Grid.Row centered>
             <Grid.Column widescreen='10' mobile='12'>
-            <Segment color='red' textAlign='center'>
-              <h1>Fire Emergency Analyser 🚀</h1>
-            </Segment>
-          </Grid.Column>
+              <Segment color='red' textAlign='center'>
+                <h1>Fire Emergency Analyser 🚀</h1>
+              </Segment>
+            </Grid.Column>
           </Grid.Row>
 
           <Grid.Row centered>
             <Grid.Column widescreen='5' textAlign='center'>
-              <Statistic value='5' label='females' color='red' />
+              <Statistic value='2' label='females' color='red' />
             </Grid.Column>
             <Grid.Column widescreen='5' textAlign='center'>
-            <Statistic value='3' label='males' color='red' />
+              <Statistic value='1' label='males' color='red' />
             </Grid.Column>
           </Grid.Row>
           <Grid.Row centered>
-            <Grid.Column widescreen='10' textAlign='center' >
-              <Segment color='red'><h1><span> Floor 1 At Fire 🔥</span> </h1></Segment>
+            <Grid.Column widescreen='12' textAlign='center' >
+              <Segment color='red'><h1><span> Floor 1 at Fire 🔥</span> </h1></Segment>
             </Grid.Column>
           </Grid.Row>
 
-          <Grid.Row>
-            
+          <Grid.Row centered>
+            <Grid.Column widescreen='10'>
+            <Card.Group>
+            <Card >
+              <Card.Content>
+                <Image floated='right' size='mini' src='https://react.semantic-ui.com/assets/images/avatar/large/steve.jpg' />
+                <Card.Header>
+                  Govind Chaudhary
+        </Card.Header>
+                <Card.Meta>
+                  Friend of Harsh
+        </Card.Meta>
+              </Card.Content>
+              <Card.Content extra>
+                <div className='ui two buttons'>
+                  <Button basic color='green'>Call</Button>
+                  <Button basic color='red'>Address</Button>
+                </div>
+              </Card.Content>
+            </Card>
+
+            <Card >
+              <Card.Content>
+                <Image floated='right' size='mini' src='https://react.semantic-ui.com/assets/images/avatar/large/molly.png' />
+                <Card.Header>
+                  Harshita
+        </Card.Header>
+                <Card.Meta>
+                  Friends of Elliot
+        </Card.Meta>
+  
+              </Card.Content>
+              <Card.Content extra>
+                <div className='ui two buttons'>
+                  <Button basic color='green'>Call</Button>
+                  <Button basic color='red'>Address</Button>
+                </div>
+              </Card.Content>
+            </Card>
+            <Card >
+              <Card.Content>
+                <Image floated='right' size='mini' src='https://react.semantic-ui.com/assets/images/avatar/large/steve.jpg' />
+                <Card.Header>
+                  Rashmi Chauhan
+        </Card.Header>
+                <Card.Meta>
+                  Friend of Harsh
+        </Card.Meta>
+              
+              </Card.Content>
+              <Card.Content extra>
+                <div className='ui two buttons'>
+                  <Button basic color='green'>Call</Button>
+                  <Button basic color='red'>Address</Button>
+                </div>
+              </Card.Content>
+            </Card>
+            </Card.Group>
+            </Grid.Column>
           </Grid.Row>
         </Grid>
 
