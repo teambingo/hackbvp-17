0cf3d82e2d614e02b93f379eead7470fd432e6c2
Ujjwal <ujjwalupadhyay8@gmail.com>
2017-10-11 02:09:36 +0530

    add

---
+++ Twilio-Implementation/re.js
@@ -0,0 +1,11 @@
+var accountSid = 'AC618185642d67dd2de832b3250097abfa';
+var authToken = "da4e5b82134ddff109dc36c5be375f78";
+var client = require('twilio')(accountSid, authToken);
+
+client.calls.create({
+    url: "http://demo.twilio.com/docs/voice.xml",
+    to: "+918800752205",
+    from: "+18134524968"
+}, function(err, call) {
+    console.log(call.sid);
+});
