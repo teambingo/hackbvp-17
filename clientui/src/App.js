87a3a3ff81451d8e49a44d86434f8229936791f7
Harshul Sharma <harshulsharma000@outlook.com>
2017-10-10 19:44:57 +0530

    fires integrated

--- clientui/src/App.js
+++ clientui/src/App.js
@@ -4,14 +4,29 @@ import fire from './fire.png';
 import './App.css';
 
 class App extends Component {
+  state = { showExtraFire:false};
+  componentWillMount() {
+    setTimeout(() => { this.setState({showExtraFire: true}); }, 3000);
+  }
+  extraFire() {
+    if(this.state.showExtraFire == true) {
+      return(
+        <div>
+          <img src={fire} className="Fire-imge"  alt="logo" />
+          <img src={fire} className="Fire-imge" alt="funky" />
+        </div>
+      );  
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          Testing title here!
+          BURN! BURN! BURN like HELL!
         </header>
         <img src={fp} className="App-logo" alt="logo" />
         <img src={fire} className="Fire-img" alt="funky" />
+        {this.extraFire()}
         <h1 className="App-title">Welcome to React</h1>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
